e71b8fbd439e89b06b1b424987179276875e0930
manny1001 <emmanuelmuya@gmail.com>
2021-01-23 11:59:34 +0200

    login

--- Screens/PhoneAuth.js
+++ Screens/PhoneAuth.js
@@ -1,27 +1,61 @@
 import React, { useState, lazy } from "react";
-import { View, Image } from "react-native";
+import { View, Text } from "react-native";
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
 } from "react-native-responsive-screen";
 import TextInput from "../Components/TextInput";
-import { RFValue } from "react-native-responsive-fontsize";
-
+import { RFPercentage, RFValue } from "react-native-responsive-fontsize";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import Modal from "modal-enhanced-react-native-web";
 import { ContextConsumer } from "../Context";
-const VerificationModal = lazy(() => import("../Components/VerificationModal"));
+import { useMutation } from "@apollo/client";
+import {
+  CodeField,
+  Cursor,
+  useBlurOnFulfill,
+  useClearByFocusCell,
+} from "react-native-confirmation-code-field";
 const BigButton = lazy(() => import("../Components/Buttons"));
 const PhoneAuthImage = lazy(() => import("../Components/PhoneAuthImage"));
-
 const PhoneAuth = (props) => {
+  const [OTP, setOTP] = React.useState("4545");
   const [cellphone, setcellphone] = React.useState(1451651515);
   const [visibleModal, setvisibleModal] = React.useState(true);
-  const [OTP, setOTP] = React.useState("4545");
-  const [OTPHandlerisEqual, setOTPHandlerisEqual] = useState(false);
+  const CELL_COUNT = 4;
+  const [value, setValue] = useState("");
+  const ref = useBlurOnFulfill({ value, cellCount: CELL_COUNT });
+  const [cellOnLayout, getCellOnLayoutHandler] = useClearByFocusCell({
+    value,
+    setValue,
+  });
+  /* const LOGIN_MUTATION = gql`
+    mutation LoginMutation($cellphone: String!, $otp: String!) {
+      login(cellphone: $cellphone, otp: $otp) {
+        token
+        user {
+          username
+        }
+      }
+    }
+  `;
 
-  const OTPHandler = (val) => {
-    setOTPHandlerisEqual(val);
+  const [login, { loading, error, data }] = useMutation(LOGIN_MUTATION, {
+    onError: (error) => {
+      console.log(loading, error);
+    },
+    onCompleted: () => {
+      console.log(data);
+    },
+  }); */
+  const DelayedFunction = ({ value }) => {
+    {
+      OTP === value
+        ? console.log("its equal")
+        : console.log("its NOOOOT equal");
+    }
+    return null;
   };
-
   return (
     <View
       style={{
@@ -29,16 +63,113 @@ const PhoneAuth = (props) => {
         justifyContent: "space-around",
       }}
     >
-      <VerificationModal
-        props={props}
-        OTPHandlerisEqual={OTPHandlerisEqual}
-        visibleModal={visibleModal}
-        cellphone={cellphone}
-        OTP={OTP}
-        OTPHandler={OTPHandler}
-        setvisibleModal={setvisibleModal}
-      />
+      <DelayedFunction OTP={OTP} value={value} />
+      <Modal
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignSelf: "center",
+          alignContent: "center",
+        }}
+        isVisible={visibleModal}
+      >
+        <View
+          style={{
+            flex: 1,
+            alignItems: "center",
+          }}
+        >
+          <Text
+            style={{
+              fontSize: RFPercentage(3),
+              alignSelf: "center",
+              color: "white",
+            }}
+          >
+            OTP is: 4545
+          </Text>
+          <Text style={{ textAlign: "center", fontSize: 30, color: "white" }}>
+            Verification Code
+          </Text>
+          <Text
+            style={{
+              alignSelf: "center",
+              color: "white",
+              fontSize: RFPercentage(1.5),
+            }}
+          >
+            Enter the OTP sent to the mobile number *******7765
+          </Text>
+          <CodeField
+            ref={ref}
+            {...cellOnLayout}
+            value={value}
+            onChangeText={(value) => setValue(value)}
+            cellCount={CELL_COUNT}
+            rootStyle={{
+              width: wp(80),
 
+              height: hp(10),
+            }}
+            keyboardType="number-pad"
+            textContentType="oneTimeCode"
+            renderCell={({ index, symbol, isFocused }) => (
+              <View
+                onLayout={getCellOnLayoutHandler(index)}
+                key={index}
+                style={[
+                  {
+                    width: wp(15),
+                    height: hp(10),
+                    backgroundColor: "geeen",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    borderBottomColor: "white",
+                    borderBottomWidth: 5,
+                  },
+                  isFocused && {
+                    borderBottomColor: "#007AFF",
+                    borderBottomWidth: 2,
+                  },
+                ]}
+              >
+                <Text
+                  style={{
+                    color: "white",
+                    fontSize: 36,
+                    textAlign: "center",
+                  }}
+                >
+                  {symbol || (isFocused ? <Cursor /> : null)}
+                </Text>
+              </View>
+            )}
+          />
+        </View>
+
+        {/* <View
+          style={{
+            height: hp(20),
+            width: wp(80),
+            justifyContent: "center",
+            position: "absolute",
+            bottom: hp(10),
+            alignSelf: "center",
+          }}
+        >
+          <Text
+            style={{
+              alignSelf: "center",
+              color: "black",
+              fontWeight: "800",
+              fontSize: RFPercentage(2),
+              marginTop: hp(5),
+            }}
+          >
+            Incorrect otp,Please try again.
+          </Text>
+        </View> */}
+      </Modal>
       <PhoneAuthImage />
 
       <TextInput
@@ -56,19 +187,13 @@ const PhoneAuth = (props) => {
         onChangeText={(text) => setcellphone(text)}
       />
 
-      <ContextConsumer>
-        {(context) => {
-          return (
-            <BigButton
-              disabled={cellphone.length === 10 ? false : true}
-              onPress={() => {
-                setvisibleModal(true);
-              }}
-              title={"Sign In"}
-            />
-          );
+      <BigButton
+        disabled={cellphone.length === 10 ? false : true}
+        onPress={() => {
+          setvisibleModal(true);
         }}
-      </ContextConsumer>
+        title={"Sign In"}
+      />
     </View>
   );
 };
